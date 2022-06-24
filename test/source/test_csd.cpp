6aae262f5aa1814dbb5f4a302d59f8f1ed05f4d4
Wai-Shing Luk <luk036@gmail.com>
2022-06-24 19:19:26 +0800

    replace CHECK with CHECK_EQ

--- test/source/test_csd.cpp
+++ test/source/test_csd.cpp
@@ -1,4 +1,4 @@
-#include <doctest/doctest.h>  // for ResultBuilder, CHECK, TEST_CASE
+#include <doctest/doctest.h>  // for ResultBuilder, CHECK_EQ, TEST_CASE
 
 #include <iosfwd>  // for string
 #include <string>  // for basic_string, operator==, operator<<
@@ -8,16 +8,16 @@ extern auto to_decimal(const std::string& csd_str) -> double;
 extern auto to_csdfixed(double num, unsigned int nnz) -> std::string;
 
 TEST_CASE("test to_csd") {
-    CHECK(to_csd(28.5, 2) == "+00-00.+0");
-    CHECK(to_csd(-0.5, 2) == "0.-0");
+    CHECK_EQ(to_csd(28.5, 2), "+00-00.+0");
+    CHECK_EQ(to_csd(-0.5, 2), "0.-0");
 }
 
 TEST_CASE("test to_decimal") {
-    CHECK(to_decimal("+00-00.+") == 28.5);
-    CHECK(to_decimal("0.-") == -0.5);
+    CHECK_EQ(to_decimal("+00-00.+"), 28.5);
+    CHECK_EQ(to_decimal("0.-"), -0.5);
 }
 
 TEST_CASE("test to_csdfixed") {
-    CHECK(to_csdfixed(28.5, 4) == "+00-00.+");
-    CHECK(to_csdfixed(-0.5, 4) == "0.-");
+    CHECK_EQ(to_csdfixed(28.5, 4), "+00-00.+");
+    CHECK_EQ(to_csdfixed(-0.5, 4), "0.-");
 }
