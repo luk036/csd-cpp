554e05bef09a182861e5c29a6da63bed1a729459
Wai-Shing Luk <luk036@gmail.com>
2023-02-15 13:15:34 +0800

    apply fix-format

--- bench/BM_switch.cpp
+++ bench/BM_switch.cpp
@@ -4,27 +4,27 @@
 
 using namespace csd;
 
-extern auto to_decimal(const std::string& csd_str) -> double;
-extern auto to_decimal_using_switch(const std::string& csd_str) -> double;
+extern auto to_decimal(const std::string &csd_str) -> double;
+extern auto to_decimal_using_switch(const std::string &csd_str) -> double;
 
-static void using_if_else(benchmark::State& state) {
-    // Code inside this loop is measured repeatedly
-    for (auto _ : state) {
-        auto result = to_decimal("+00-00+00+0-0+0+.+00+00-0++");
-        // Make sure the variable is not optimized away by compiler
-        benchmark::DoNotOptimize(result);
-    }
+static void using_if_else(benchmark::State &state) {
+  // Code inside this loop is measured repeatedly
+  for (auto _ : state) {
+    auto result = to_decimal("+00-00+00+0-0+0+.+00+00-0++");
+    // Make sure the variable is not optimized away by compiler
+    benchmark::DoNotOptimize(result);
+  }
 }
 // Register the function as a benchmark
 BENCHMARK(using_if_else);
 
-static void using_switch(benchmark::State& state) {
-    // Code inside this loop is measured repeatedly
-    for (auto _ : state) {
-        auto result = to_decimal_using_switch("+00-00+00+0-0+0+.+00+00-0++");
-        // Make sure the variable is not optimized away by compiler
-        benchmark::DoNotOptimize(result);
-    }
+static void using_switch(benchmark::State &state) {
+  // Code inside this loop is measured repeatedly
+  for (auto _ : state) {
+    auto result = to_decimal_using_switch("+00-00+00+0-0+0+.+00+00-0++");
+    // Make sure the variable is not optimized away by compiler
+    benchmark::DoNotOptimize(result);
+  }
 }
 BENCHMARK(using_switch);
 
