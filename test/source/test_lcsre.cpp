162cf7698e2ca8219670892bcd6a6384bae336cb
Wai-Shing Luk <luk036@gmail.com>
2024-06-20 13:11:22 +0000

    add a test case for lcsre

--- test/source/test_lcsre.cpp
+++ test/source/test_lcsre.cpp
@@ -7,4 +7,5 @@ using namespace csd;
 TEST_CASE("test lcsre") {  // Driver Code
     CHECK_EQ(longest_repeated_substring("+-00+-00+-00+-0", 15U), "+-00+-0");
     CHECK_EQ(longest_repeated_substring("abcdefghijklmno", 15U), "");
+    CHECK_EQ(longest_repeated_substring("banana", 6U), "an");
 }
